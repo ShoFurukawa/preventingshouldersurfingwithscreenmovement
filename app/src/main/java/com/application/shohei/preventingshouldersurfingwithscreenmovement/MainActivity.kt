9f5075fdb0c18df7f66cf921c657ec25a9651447
ShoFurukawa <al15082@shibaura-it.ac.jp>
2018-11-29 16:20:01 +0900

    パスワード入力用のEditTextを追加し、文字数を４に制限した。パスワード設定用の画面のactivityを追加(何も書いてない)

--- app/src/main/java/com/application/shohei/preventingshouldersurfingwithscreenmovement/MainActivity.kt
+++ app/src/main/java/com/application/shohei/preventingshouldersurfingwithscreenmovement/MainActivity.kt
@@ -2,12 +2,20 @@ package com.application.shohei.preventingshouldersurfingwithscreenmovement
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //EditTextでキーボード入力が出ないようにした
+        val editText:EditText = findViewById(R.id.editText)
+        editText.keyListener = null
+
 
     }
 }
---
+++ app/src/main/java/com/application/shohei/preventingshouldersurfingwithscreenmovement/SetPassword.kt
@@ -0,0 +1,12 @@
+package com.application.shohei.preventingshouldersurfingwithscreenmovement
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SetPassword : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_password)
+    }
+}
